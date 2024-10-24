902d8f40f9f870bec3cefefceb28794afff1deb3
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-24 17:29:14 +0800

    Updated main, changed pseudocode arround, trying out different concepts

--- name_and_age_array_main.py
+++ name_and_age_array_main.py
@@ -1,6 +1,33 @@
-#initialize array and variables
 #initialize functions
-#input values using a for loop
+#function that checks if the name is valid
+#the function returns an error message if the name is invalid
+def valid_name(name):
+    
+
+
+#function that checks if the age is valid
+#the function returns an error message if the age is invalid
+def valid_age(age):
+    try:
+        age = int(age)
+        return age <= 1 and age <= 100
+    except:
+        print("Invalid input")
+
+#funnction that asks the user to input a valid name and age
+def user_input():
+
+    while True:
+        try:
+            name = input("Enter your name: ")
+            age = int(input("Enter your age: "))
+        except:
+            print("Invalid input")
+            
+
+        
+    
+
+
 #call the functions
 #display the output
-
