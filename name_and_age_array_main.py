5858445cbeeaa81f736f793bdff9608e4652d6e7
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-25 19:01:23 +0800

    Updated Main, changed pseudocode and changed my approach

--- name_and_age_array_main.py
+++ name_and_age_array_main.py
@@ -1,33 +1,18 @@
-#initialize functions
-#function that checks if the name is valid
-#the function returns an error message if the name is invalid
-def valid_name(name):
-    
+#initialize the dictionary
+bio_id = {}
 
+#loop that prompts the user for input
+while True:
+    #Loop that retries when a user enters an invalid input
+        while True:
+            try:
+                name = input("Please enter a name: ")
+                age = int(input("Please enter an age"))
 
-#function that checks if the age is valid
-#the function returns an error message if the age is invalid
-def valid_age(age):
-    try:
-        age = int(age)
-        return age <= 1 and age <= 100
-    except:
-        print("Invalid input")
+                retry = input("Would you want to try again? ")
 
-#funnction that asks the user to input a valid name and age
-def user_input():
-
-    while True:
-        try:
-            name = input("Enter your name: ")
-            age = int(input("Enter your age: "))
-        except:
-            print("Invalid input")
+                #Stops the nested loop
+                break
             
-
-        
-    
-
-
-#call the functions
-#display the output
+            except: 
+                print("Invalid input")
